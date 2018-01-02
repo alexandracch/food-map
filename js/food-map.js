dce0d1ec73cf9fe63e3137447e254123705ca048
alexandracch <alexa_theprinces@hotmail.com>
2018-01-02 12:20:34 -0500

    comentario

--- js/food-map.js
+++ js/food-map.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
     // evento change
     $select.on('change', function() {
         var optionSelect = $(this).val();//captura option
-        console.log($(this).val());
+        // console.log($(this).val());
         if (optionSelect === 'peruvian') {
             $peruvianFood.addClass('block')
           } else if (optionSelect === 'mexican') {
