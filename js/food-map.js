b4aae8904d45e6a69597a80d1d058bedff2f493d
alexandracch <alexa_theprinces@hotmail.com>
2018-01-02 12:21:40 -0500

    guia de estilos

--- js/food-map.js
+++ js/food-map.js
@@ -1,29 +1,28 @@
-$(document).ready(function(){
-    //selecciona select
-    var $select = $('select');
-    //capturo section
-    var $peruvianFood = $('#op-peruvian-food');
-    var $mexicanFood = $('#op-mexican-food');
-    var $fastFood = $('#op-fast-food');
-    var $italianFood = $('#op-italian-food');
-    // console.log($peruvianFood);
-    // console.log($mexicanFood);
-    // console.log($fastFood);
-    // console.log($italianFood);
+$(document).ready(function() {
+  // selecciona select
+  var $select = $('select');
+  // capturo section
+  var $peruvianFood = $('#op-peruvian-food');
+  var $mexicanFood = $('#op-mexican-food');
+  var $fastFood = $('#op-fast-food');
+  var $italianFood = $('#op-italian-food');
+  // console.log($peruvianFood);
+  // console.log($mexicanFood);
+  // console.log($fastFood);
+  // console.log($italianFood);
 
-    // evento change
-    $select.on('change', function() {
-        var optionSelect = $(this).val();//captura option
-        // console.log($(this).val());
-        if (optionSelect === 'peruvian') {
-            $peruvianFood.addClass('block')
-          } else if (optionSelect === 'mexican') {
-            $mexicanFood.addClass('block');
-          } else if (optionSelect === 'fastfood'){
-            $fastFood.addClass('block');
-          } else if (optionSelect === 'italian'){
-            $fastFood.addClass('block');
-          }
-        });
-      
-      });
+  // evento change
+  $select.on('change', function() {
+    var optionSelect = $(this).val();// captura option
+    // console.log($(this).val());
+    if (optionSelect === 'peruvian') {
+      $peruvianFood.addClass('block');
+    } else if (optionSelect === 'mexican') {
+      $mexicanFood.addClass('block');
+    } else if (optionSelect === 'fastfood') {
+      $fastFood.addClass('block');
+    } else if (optionSelect === 'italian') {
+      $fastFood.addClass('block');
+    }
+  });
+});
